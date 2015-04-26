7ab8fa437bd71ba3d00560b7ca7bf5b01ceb0d41
tanitta <ttata.trit@gmail.com>
2015-04-26 23:25:33 +0900

    change method name(test->move)

--- public/app.js
+++ public/app.js
@@ -4,17 +4,14 @@ var context = canvas.getContext("2d");
 
 context.strokeStyle = "#000";
 context.fillStyle = "#F66";
-document.addEventListener('mousedown', down);
-document.addEventListener('mousemove', test);
-document.addEventListener('mouseup', up);
 // document.addEventListener('mousedown', init);
 var is_down = false;
 var pos_x = 0;
 var pos_y = 0;
 
-
-// function init(){
-// }
+document.addEventListener('mousedown', down);
+document.addEventListener('mousemove', move);
+document.addEventListener('mouseup', up);
 
 onload=function(){
 	var img = new Image();
@@ -29,7 +26,7 @@ onload=function(){
 function down(){
 	is_down = true;
 }
-function test(e) {
+function move(e) {
 	canvasRect = canvas.getBoundingClientRect();
 	if(is_down){
 		io.push("hello", pos_x+","+pos_y+","+(e.clientX-canvasRect.left)+","+(e.clientY-canvasRect.top));
@@ -53,14 +50,14 @@ function draw(x1,y1,x2,y2,width,color){
 }
 
 io.on("echo", function(message){
-   p = message.split(",");
-   draw(parseFloat(p[0]),parseFloat(p[1]),parseFloat(p[2]),parseFloat(p[3]),1,"#000000");
+	p = message.split(",");
+	draw(parseFloat(p[0]),parseFloat(p[1]),parseFloat(p[2]),parseFloat(p[3]),1,"#000000");
 });
 
 
 io.on("request_image", function(message){
 	var img_png_src = canvas.toDataURL();
-	
+
 	io.push("push_image", img_png_src);
 });
 
@@ -68,7 +65,7 @@ io.on("requested_image", function(message){
 	console.log("start load log");
 	var requested_img = new Image();
 	// console.log(message.message);
-	
+
 	requested_img.src = message.message;
 	requested_img.onload = function() {
 		console.log("loaded log");
